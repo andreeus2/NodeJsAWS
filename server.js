848df05493c915cc2c12cfb0b0b468405482ef5b
andreeus2 <andreeus2@gmail.com>
2021-10-27 21:54:10 -0500

    html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app  = express();
 
-app.get('/' ,(req, res)  => res.send("Hello World whit express"))
+app.get('/' ,(req, res)  => res.send("<h1>Express con HTLM</h1>"))
 
 app.listen(3000);
 console.log('server on port 3000');
